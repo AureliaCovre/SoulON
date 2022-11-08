8d12c0b682b198dcf8316dee6fe18bf9a5f52000
AureliaCovre <93683314+AureliaCovre@users.noreply.github.com>
2022-11-08 14:50:34 -0300

    Create exercicios.py

---
+++ PYTHON_1/exercicios.py
@@ -0,0 +1,45 @@
+#Valores baseados no meu pet
+'''
+Aqui temos variaveis de animais contendo nome, quantidade, peso e se é vacinado 
+'''
+
+#nomes_animais = "nina"
+#qtd_animais = 1
+#peso = 5.658
+#vacina = True
+#--------------------------------------------------------------------------------------  
+#EXERCICIO 2
+#nome = input("Insira seu nome: ")
+#altura = float(input("Sua altura?  "))
+#cpf = int(input("digite seu cpf:  "))  
+#print("meu nome é  ", nome, "minha altura  ", altura, "meu cpf ", cpf)           
+
+#--------------------------------------------------------------------------------------  
+#EXERCICIO 3
+#numero = 11
+#if numero == 15:
+ #   print("numero igual a 15")
+#elif numero > 15:
+ #   print("numero maior que 15")
+#elif numero < 15:
+ #   print("numero menor que 15")    
+    
+ #--------------------------------------------------------------------------------------  
+ #EXERCICIO 4
+ #Crie um laço com for in range que imprima os múltiplos de 3 no intervalo entre 0 e 15.
+ 
+#for i in range(0,18,3):
+    #print(i)
+
+#--------------------------------------------------------------------------------------   
+  #EXERCICIO 5
+  #1-Crie uma string contendo o alfabeto com letras maiúsculas separado por espaços.
+  #2-Percorra e imprima essa string com enumerate.
+  #3-Substitua os espaços por traço. “-” e imprima para o usuário.
+  
+#alfabeto = "A B C D E F G H I J K L M N O P Q R S T U V X Z W"
+
+#for k,v in enumerate(alfabeto):
+#  print(k,v)
+  
+#print(alfabeto.replace(" ", "-"))  
